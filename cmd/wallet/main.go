d96db1c8106984a475821e39f4af57803a2b1545
Sebastian Bolanos <sebasheston@protonmail.com>
2020-03-11 21:44:36 +0000

    Used Meson-clients instead of katzenpost client

--- cmd/wallet/main.go
+++ cmd/wallet/main.go
@@ -1,79 +1,54 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 
-	"github.com/hashcloak/Meson-plugin/pkg/common"
+	client "github.com/hashcloak/Meson-client"
 	"github.com/hashcloak/Meson-wallet-demo/pkg/ethers"
-	"github.com/katzenpost/client"
-	"github.com/katzenpost/client/config"
 )
 
 func main() {
 	cfgFile := flag.String("c", "client.toml", "Path to the server config file")
 	ticker := flag.String("t", "", "Ticker")
-	chainID := flag.Int("chain", 1, "Chain ID for specific ETH-based chain")
-	service := flag.String("s", "", "Service Name")
+	//service := flag.String("s", "", "Service Name")
 	rawTransactionBlob := flag.String("rt", "", "Raw Transaction blob to send over the network")
 	privKey := flag.String("pk", "", "Private key used to sign the txn")
 	rpcEndpoint := flag.String("rpc", "https://goerli.hashcloak.com", "Ethereum rpc endpoint")
 	flag.Parse()
 
-	cfg, err := config.LoadFile(*cfgFile)
+	client, err := client.New(*cfgFile, *ticker)
 	if err != nil {
-		panic(err)
-	}
-
-	cfg, linkKey := client.AutoRegisterRandomClient(cfg)
-	c, err := client.New(cfg)
-	if err != nil {
-		panic(err)
-	}
-
-	session, err := c.NewSession(linkKey)
-	if err != nil {
-		panic(err)
+		panic("ERROR In creating new client: " + err.Error())
 	}
 
 	if *rawTransactionBlob == "" {
 		if *privKey == "" {
 			panic("must specify a transaction blob in hex or a private key to sign a txn")
 		}
-		rawTransactionBlob, err = produceSignedRawTxn(privKey, rpcEndpoint, chainID)
+		rawTransactionBlob, err = produceSignedRawTxn(privKey, rpcEndpoint)
 		if err != nil {
 			panic("Raw txn error: " + err.Error())
 		}
 	}
 
-	// serialize our transaction inside a eth kaetzpost request message
-	req := common.NewRequest(*ticker, *rawTransactionBlob)
-	mesonRequest := req.ToJson()
-
-	mesonService, err := session.GetService(*service)
+	client.Start()
+	reply, err := client.SendRawTransaction(rawTransactionBlob, ticker)
 	if err != nil {
-		panic("Client error" + err.Error())
+		panic("ERROR Send raw transaction: " + err.Error())
 	}
 
-	reply, err := session.BlockingSendUnreliableMessage(mesonService.Name, mesonService.Provider, mesonRequest)
-	if err != nil {
-		panic("Meson Request Error" + err.Error())
-	}
 	fmt.Printf("reply: %s\n", reply)
 	fmt.Println("Done. Shutting down.")
-	c.Shutdown()
+	client.Shutdown()
 }
 
-func produceSignedRawTxn(pk *string, rpcEndpoint *string, chainID *int) (*string, error) {
+func produceSignedRawTxn(pk *string, rpcEndpoint *string) (*string, error) {
 	ethers, err := ethers.SetURLAndChainID(*rpcEndpoint)
 	if err != nil {
 		return nil, err
 	}
 
-	if ethers.ChainID.Int64() != int64(*chainID) {
-		return nil, errors.New("ChainIDs are not the same between rpcEndpoint and chainID flag")
-	}
 	rawTxn, err := ethers.GenerateSignedRawTxn(*pk)
 	return rawTxn, nil
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/hashcloak/Meson-wallet-demo
 go 1.13
 
 require (
-	github.com/ethereum/go-ethereum v1.9.8
-	github.com/hashcloak/Meson-client v0.0.0-20191203233537-dc6d4bdd3049
-	github.com/hashcloak/Meson-plugin v0.0.0-20200122032451-3f486b0607e5
-	github.com/katzenpost/client v0.0.3-0.20191109165001-aa02bb21ca21
+	github.com/ethereum/go-ethereum v1.9.10
+	github.com/hashcloak/Meson-client v0.0.0-20200207001411-1358077b8645
 )
+
+replace github.com/hashcloak/Meson-client => /home/sebas/repos/hashcloack/Meson-client
